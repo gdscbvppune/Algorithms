119b944c1b59162a03614d9489af62cf548b3ae4
thegeekysk <91889678+thegeekysk@users.noreply.github.com>
2021-10-05 20:40:33 +0530

    Update Shivam Insertion_Sort.java

--- Sorting/Shivam Insertion_Sort.java
+++ Sorting/Shivam Insertion_Sort.java
@@ -1,58 +1,42 @@
-// C++ program for insertion sort
-#include <bits/stdc++.h>
-using namespace std;
- 
-/* Function to sort an array using insertion sort*/
-void insertionSort(int arr[], int n)
-{
-    int i, key, j;
-    for (i = 1; i < n; i++)
+// Java program for implementation of Insertion Sort
+class InsertionSort {
+    /*Function to sort array using insertion sort*/
+    void sort(int arr[])
     {
-        key = arr[i];
-        j = i - 1;
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
  
-        /* Move elements of arr[0..i-1], that are
-        greater than key, to one position ahead
-        of their current position */
-        while (j >= 0 && arr[j] > key)
-        {
-            arr[j + 1] = arr[j];
-            j = j - 1;
+            /* Move elements of arr[0..i-1], that are
+               greater than key, to one position ahead
+               of their current position */
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
         }
-        arr[j + 1] = key;
+    }
+ 
+    /* A utility function to print array of size n*/
+    static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+ 
+        System.out.println();
+    }
+ 
+    // Driver method
+    public static void main(String args[])
+    {
+        int arr[] = { 12, 11, 13, 5, 6 };
+ 
+        InsertionSort ob = new InsertionSort();
+        ob.sort(arr);
+ 
+        printArray(arr);
     }
 }
- 
-// A utility function to print an array of size n
-void printArray(int arr[], int n)
-{
-    int i;
-    for (i = 0; i < n; i++)
-        cout << arr[i] << " ";
-    cout << endl;
-}
- 
-/* Driver code */
-int main()
-{
-    int arr[] = { 12, 11, 13, 5, 6 };
-    int n = sizeof(arr) / sizeof(arr[0]);
- 
-    insertionSort(arr, n);
-    printArray(arr, n);
- 
-    return 0;
-}
-/* Driver code */
-int main()
-{
-    int arr[] = { 12, 11, 13, 5, 6 };
-    int n = sizeof(arr) / sizeof(arr[0]);
- 
-    insertionSort(arr, n);
-    printArray(arr, n);
- 
-    return 0;
-}
- 
-// This is code is contributed by Shivam Kumar Pandey
